c019e959defb3b5011d1cf0f7e851f3ed4b2ceb0
Jordan Wesson <s201031@students.aie.edu.au>
2021-03-03 16:23:33 +1100

    22:55

---
+++ PacMan/PacMan/GameLevelScreen.cs
@@ -0,0 +1,32 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PacMan
+{
+    enum TileType
+    {
+        EMPTY,
+        WALL,
+        DOT
+    }
+
+    class GameLevelScreen : IGameState
+    {
+        public GameLevelScreen(Program program) : base(program)
+        {
+
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Draw()
+        {
+            Raylib.DrawText("Game Level", 10, 10, 20, Color.WHITE);
+        }
+    }
+}
---
+++ PacMan/PacMan/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Raylib_cs;
+
+namespace PacMan
+{
+    class Program
+    {
+        int windowWidth = 800;
+        int windowHeight = 450;
+        string windowTitle = "Pacman";
+
+        IGameState gameState;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.RunGame();
+        }
+
+        void RunGame()
+        {
+            Raylib.InitWindow(windowWidth, windowHeight, windowTitle);
+            Raylib.SetTargetFPS(60);
+
+            LoadGame();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Update();
+                Draw();
+            }
+
+            Raylib.CloseWindow();
+        }
+
+        void LoadGame()
+        {
+            gameState = new GameLevelScreen(this);
+        }
+
+        void Update()
+        {
+            if (gameState != null)
+                gameState.Update();
+        }
+
+        void Draw()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+
+            if (gameState != null)
+                gameState.Draw();
+
+            Raylib.DrawFPS(10, windowHeight - 20);
+            Raylib.EndDrawing();
+        }
+    }
+}
